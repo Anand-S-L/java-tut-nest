ef41bf4be63f92a326686e7a6c074f57aca7588a
Anand-S-L <anandslperoorkada@gmail.com>
2022-07-25 16:07:25 +0530

    swap

---
+++ src/swap.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a");
+        int a=s.nextInt();
+        System.out.println("Enter b");
+        int b=s.nextInt();
+        int temp=0;
+
+        temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+    }
+}
